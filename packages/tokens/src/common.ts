0687d9860c54e2ac5dd7f498898093a9d821e97a
Streams <launchstream.xyz@gmail.com>
2025-08-17 20:11:41 -0700

    Update common.ts

--- packages/tokens/src/common.ts
+++ packages/tokens/src/common.ts
@@ -10,7 +10,7 @@ export const SST_MAINNET = new ERC20Token(
 )
 export const SST_TESTNET = new ERC20Token(
   ChainId.RINKEBY, // sepolia
-  '0x3169e28FC7c35CF7841Fbf008F5BE3CF339eb400',
+  '0xF8e1a71E51f0937CE46D76795A74bc78D192c534',
   'SST',
   'Swap Stream Token',
   'https://swapstream.finance',
