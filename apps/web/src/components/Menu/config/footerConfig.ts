a80136fd579c113cad74886dd97fe3c9ae8e88e2
Streams <launchstream.xyz@gmail.com>
2025-08-22 22:53:28 -0700

    Update footerConfig.ts

--- apps/web/src/components/Menu/config/footerConfig.ts
+++ apps/web/src/components/Menu/config/footerConfig.ts
@@ -12,7 +12,7 @@ export const footerLinks: (t: ContextApi['t']) => FooterLinkType[] = (t) => [
       },
       {
         label: t('Whitepaper'),
-        href: '/swapstream.finance/whitepaper.pdf',
+        href: '/whitepaper.pdf',
       },
     ],
   },
